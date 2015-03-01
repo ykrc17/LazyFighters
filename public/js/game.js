6150546bf6a6393e9ef7b9a2ef102060835631f9
yk <ykrc17@163.com>
2015-03-01 22:43:50 +0800

    加入5X5界面

--- public/js/game.js
+++ public/js/game.js
@@ -3,6 +3,14 @@ $(function() {
   var position = null
 
   // target
+  for(var i=-2; i<=2; i++) {
+    for(var j=-2; j<=2; j++) {
+      $("#a"+j+"b"+i).attr({x: j,y: i})
+      $("#a"+j+"b"+i).click(function() {
+        socket.emit("target", {x:position.x+Number($(this).attr("x")), y:position.y+Number($(this).attr("y"))})
+      })
+    }
+  }
   var targetLeft = $('#target-left')
   var targetUp = $('#target-up')
   var targetDown = $('#target-down')
